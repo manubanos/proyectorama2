1c8f5d2adef51998504901bf42cd8d1e32c3bc2c
Manu <manubanos@gmail.com>
2019-01-07 19:56:07 +0100

    he modicado el archivo holamundo.c

--- holamundo.c
+++ holamundo.c
@@ -3,5 +3,12 @@
 int main()
 {
         printf("Hola mundo");
+        printf("Que tal");
+        printf("Que tal");
+
+        printf("Que tal");
+
+        printf("Que tal");
+
         return 0;
 }
